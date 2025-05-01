0b66fe8cb0343324269682e75467cea9c2358c27
hseun <hseun0732@dms.hs.kr>
2025-05-01 11:14:53 +0900

    db :: model

---
+++ src/prisma/migrations/20250501013548_init/migration.sql
@@ -0,0 +1,170 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('STUDENT', 'TEACHER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "StudentState" AS ENUM ('SCHOOL', 'GRADURATION', 'LEAVE', 'DROP', 'KICK');
+
+-- CreateEnum
+CREATE TYPE "ContestState" AS ENUM ('BEFORE', 'NOW', 'VOTING', 'PENDING', 'FINISHED');
+
+-- CreateEnum
+CREATE TYPE "Author" AS ENUM ('PERSONAL', 'TEAM');
+
+-- CreateEnum
+CREATE TYPE "ProjectState" AS ENUM ('WRITING', 'PENDING', 'APPROVAL', 'REJECTED', 'MODIFY');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" BIGSERIAL NOT NULL,
+    "email" VARCHAR(32) NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" VARCHAR(6) NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" BIGSERIAL NOT NULL,
+    "userId" BIGINT,
+    "generation" CHAR(2) NOT NULL,
+    "name" VARCHAR(6) NOT NULL,
+    "grade" CHAR(1),
+    "classNumber" CHAR(1),
+    "studentNumber" CHAR(2),
+    "state" "StudentState" NOT NULL DEFAULT 'SCHOOL',
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Contest" (
+    "id" BIGSERIAL NOT NULL,
+    "name" VARCHAR(30) NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "purpose" VARCHAR(300),
+    "state" "ContestState" NOT NULL DEFAULT 'BEFORE',
+
+    CONSTRAINT "Contest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" BIGSERIAL NOT NULL,
+    "contestId" BIGINT NOT NULL,
+    "writerId" BIGINT,
+    "projectName" VARCHAR(30) NOT NULL,
+    "authorCategory" "Author" NOT NULL,
+    "teamName" VARCHAR(30),
+    "skills" VARCHAR(600),
+    "introduction" VARCHAR(100),
+    "description" TEXT,
+    "startDate" DATE NOT NULL,
+    "endDate" DATE NOT NULL,
+    "image" TEXT,
+    "vedio" TEXT,
+    "state" "ProjectState" NOT NULL DEFAULT 'WRITING',
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Award" (
+    "id" BIGSERIAL NOT NULL,
+    "contestId" BIGINT NOT NULL,
+    "name" VARCHAR(24) NOT NULL,
+    "awardCount" INTEGER NOT NULL,
+
+    CONSTRAINT "Award_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AwardProject" (
+    "projectId" BIGINT NOT NULL,
+    "awardId" BIGINT NOT NULL,
+
+    CONSTRAINT "AwardProject_pkey" PRIMARY KEY ("projectId","awardId")
+);
+
+-- CreateTable
+CREATE TABLE "Member" (
+    "id" BIGSERIAL NOT NULL,
+    "studentId" BIGINT,
+    "projectId" BIGINT NOT NULL,
+
+    CONSTRAINT "Member_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" SERIAL NOT NULL,
+    "projectId" BIGINT NOT NULL,
+    "userId" BIGINT NOT NULL,
+    "rank" VARCHAR(1) NOT NULL,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Mark" (
+    "id" BIGSERIAL NOT NULL,
+    "projectId" BIGINT NOT NULL,
+    "userId" BIGINT NOT NULL,
+
+    CONSTRAINT "Mark_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "projectId" BIGINT NOT NULL,
+    "content" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("projectId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_userId_key" ON "Student"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Student" ADD CONSTRAINT "Student_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_contestId_fkey" FOREIGN KEY ("contestId") REFERENCES "Contest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_writerId_fkey" FOREIGN KEY ("writerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Award" ADD CONSTRAINT "Award_contestId_fkey" FOREIGN KEY ("contestId") REFERENCES "Contest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AwardProject" ADD CONSTRAINT "AwardProject_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AwardProject" ADD CONSTRAINT "AwardProject_awardId_fkey" FOREIGN KEY ("awardId") REFERENCES "Award"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Mark" ADD CONSTRAINT "Mark_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Mark" ADD CONSTRAINT "Mark_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
