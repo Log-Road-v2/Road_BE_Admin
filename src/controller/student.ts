f95b96e375e5149abb044b7dc015978427e80ef0
hseun <hseun0732@dms.hs.kr>
2025-05-20 19:28:50 +0900

    chore :: req 타입 변경

--- src/controller/student.ts
+++ src/controller/student.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response } from 'express';
+import express, { Response } from 'express';
 import student from '../service/student';
 import { getApiLimit } from '../middleware/limit';
 import { verifyJWT } from '../middleware/jwt';
 import { checkRight } from '../middleware/checkRight';
+import { AuthenticatedRequest } from '../types';
 
 const router = express.Router();
 
-router.get('/', getApiLimit, verifyJWT, checkRight, (req: Request, res: Response) => {
+router.get('/', getApiLimit, verifyJWT, checkRight, (req: AuthenticatedRequest, res: Response) => {
   student.searchStudents(req, res);
 });
 
