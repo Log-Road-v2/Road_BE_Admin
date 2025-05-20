4bfadb453daba28fdd956599de750b3657b4f4a3
hseun <hseun0732@dms.hs.kr>
2025-05-20 17:26:14 +0900

    rename :: postApiLimit -> apiLimit

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,21 +1,21 @@
 import express, { Request, Response } from 'express';
 import auth from '../service/auth';
-import { postApiLimit } from '../middleware/limit';
+import { apiLimit } from '../middleware/limit';
 import { verifyJWT } from '../middleware/jwt';
 import { AuthenticatedRequest } from '../types/auth';
 
 const router = express.Router();
 
-router.post('/signup', postApiLimit, (req: Request, res: Response) => {
+router.post('/signup', apiLimit, (req: Request, res: Response) => {
   auth.signUp(req, res);
 });
-router.post('/login', postApiLimit, (req: Request, res: Response) => {
+router.post('/login', apiLimit, (req: Request, res: Response) => {
   auth.login(req, res);
 });
-router.post('/refresh', postApiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) => {
+router.post('/refresh', apiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) => {
   auth.refresh(req, res);
 });
-router.post('/logout', postApiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) => {
+router.post('/logout', apiLimit, verifyJWT, (req: AuthenticatedRequest, res: Response) => {
   auth.logout(req, res);
 });
 
