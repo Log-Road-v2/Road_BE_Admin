45b56e85dad5346bc26064f8b44a5c8412f43cc9
hseun <hseun0732@dms.hs.kr>
2025-05-20 10:30:26 +0900

    chore :: postApiLimit -> getApiLinit

--- src/controller/student.ts
+++ src/controller/student.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
 import student from '../service/student';
-import { postApiLimit } from '../middleware/limit';
+import { getApiLimit } from '../middleware/limit';
 import { verifyJWT } from '../middleware/jwt';
 import { checkRight } from '../middleware/checkRight';
 
 const router = express.Router();
 
-router.get('/', postApiLimit, verifyJWT, checkRight, (req: Request, res: Response) => {
+router.get('/', getApiLimit, verifyJWT, checkRight, (req: Request, res: Response) => {
   student.searchStudents(req, res);
 });
 
