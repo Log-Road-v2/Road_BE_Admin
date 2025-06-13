a27f98c900a2162522ce6b33fa8e952bdd9e7d6e
hseun <hseun0732@dms.hs.kr>
2025-06-13 16:43:11 +0900

    fix :: uploadStudent

--- src/service/student/uploadStudent.ts
+++ src/service/student/uploadStudent.ts
@@ -17,80 +17,74 @@ export const uploadStudent = async (req: AuthenticatedRequest, res: Response<Bas
       distinct: ['generation']
     });
     const generationsNum = generations.map((g) => Number(g.generation) || 0);
-    const newGeneration = (Math.max(0, ...generationsNum) + 1).toString();
+    const newGeneration = Math.max(0, ...generationsNum) + 1;
 
     const workbook = read(req.file.buffer, { type: 'buffer' });
 
     await prisma.$transaction(async (tx) => {
+      await tx.student.updateMany({
+        where: { grade: 3 },
+        data: {
+          grade: null,
+          classNumber: null,
+          studentNumber: null,
+          state: 'GRADUATION'
+        }
+      });
+
       for (const sheetName of workbook.SheetNames) {
         const [grade, classNumber] = sheetName.split('-');
-        const numGrade = Number(grade);
+        const [numGrade, numClass] = [Number(grade), Number(classNumber)];
 
         const data = utils.sheet_to_json(workbook.Sheets[sheetName], {
           range: 3,
           defval: '',
-          header: grade === '1' ? ['studentNum', 'name'] : ['studentNum', 'name', 'gender', 'beforeStudentNum', 'etc']
+          header: numGrade === 1 ? ['studentNum', 'name'] : ['studentNum', 'name', 'gender', 'beforeStudentNum', 'etc']
         });
 
-        if (grade === '1') {
+        if (numGrade === 1) {
           const studentsData = (data as FileFirstGradeStudentData[]).map((d) => ({
             generation: newGeneration,
             name: d.name,
-            grade: '1',
-            classNumber: classNumber,
-            studentNumber: (d.studentNum % 100).toString()
+            grade: numGrade,
+            classNumber: numClass,
+            studentNumber: d.studentNum % 100
           }));
 
           await tx.student.createMany({
-            data: studentsData,
-            skipDuplicates: true
+            data: studentsData
           });
         } else {
-          await Promise.all(
-            (data as FileStudentData[]).map(async (d) => {
-              const prev = await tx.student.findFirst({
-                where: {
-                  grade: (numGrade - 1).toString(),
-                  classNumber: classNumber,
-                  studentNumber: (d.beforeStudentNum % 100).toString()
-                }
-              });
-              if (prev) {
-                // await tx.student.update({
-                //   where: {id: prev.id},
-                //   data: {
-                //     grade:
-                //   }
-                // })
-              }
-            })
-          );
-
-          for (const student of data as FileStudentData[]) {
-            const existing = await prisma.student.findFirst({
+          for (const d of data as FileStudentData[]) {
+            const beforeStudentNumStr = d.beforeStudentNum.toString().padEnd(4, '0');
+            const beforeClass = parseInt(beforeStudentNumStr.slice(1, 2));
+            const beforeNumber = parseInt(beforeStudentNumStr.slice(2, 4));
+            const studentNum = parseInt(d.studentNum.toString().padEnd(4, '0').slice(2, 4));
+            const prev = await tx.student.findFirst({
               where: {
-                grade: grade,
-                classNumber: classNumber,
-                studentNumber: (student.studentNum % 100).toString()
+                grade: numGrade - 1,
+                classNumber: beforeClass,
+                studentNumber: beforeNumber,
+                name: d.name
               }
             });
-            if (!existing) {
-              await prisma.student.create({
+            if (prev) {
+              await tx.student.update({
+                where: { id: prev.id },
                 data: {
-                  generation: (Number(newGeneration) - Number(grade)).toString(),
-                  name: student.name,
-                  grade: grade,
-                  classNumber: classNumber,
-                  studentNumber: (student.studentNum % 100).toString()
+                  grade: numGrade,
+                  classNumber: numClass,
+                  studentNumber: studentNum
                 }
               });
             } else {
-              await prisma.student.update({
-                where: { id: existing.id },
+              await tx.student.create({
                 data: {
-                  grade: grade,
-                  classNumber: classNumber,
-                  studentNumber: (student.beforeStudentNum % 100).toString()
+                  generation: newGeneration - numGrade + 1,
+                  name: d.name,
+                  grade: numGrade,
+                  classNumber: numClass,
+                  studentNumber: studentNum
                 }
               });
             }
