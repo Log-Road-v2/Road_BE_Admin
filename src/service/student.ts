e9e633af2a9d9bc6ab9f0e2747d5066d3de10f11
hseun <hseun0732@dms.hs.kr>
2025-06-13 16:49:19 +0900

    chore :: 순서 변경

--- src/service/student.ts
+++ src/service/student.ts
@@ -1,11 +1,11 @@
-import { searchStudents } from './student/searchStudents';
 import { modifyStudent } from './student/modifyStudent';
+import { searchStudents } from './student/searchStudents';
 import { removeStudent } from './student/removeStudent';
 import { uploadStudent } from './student/uploadStudent';
 
 export default {
-  searchStudents,
   modifyStudent,
   removeStudent,
+  searchStudents,
   uploadStudent
 };
