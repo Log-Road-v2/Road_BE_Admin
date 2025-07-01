ab565ce8bd044f455bd82fef94a61fee7f297954
hseun <hseun0732@dms.hs.kr>
2025-07-01 23:46:56 +0900

    feature :: modifyContest

--- src/controller/contest.ts
+++ src/controller/contest.ts
@@ -9,5 +9,6 @@ const app = express.Router();
 app.get('/', getApiLimit, verifyJWT, checkRight, contest.contestListHandler);
 app.post('/', apiLimit, verifyJWT, checkRight, contest.openContestHandler);
 app.get('/:contestId', getApiLimit, verifyJWT, checkRight, contest.contestDetailHandler);
+app.patch('/:contestId', apiLimit, verifyJWT, checkRight, contest.modifyContestHandler);
 
 export default app;
--- src/service/contest.ts
+++ src/service/contest.ts
@@ -1,9 +1,11 @@
 import { contestDetailHandler } from './contest/contestDetail';
 import { contestListHandler } from './contest/contestList';
+import { modifyContestHandler } from './contest/modifyContest';
 import { openContestHandler } from './contest/openContest';
 
 export default {
   contestDetailHandler,
   contestListHandler,
+  modifyContestHandler,
   openContestHandler
 };
